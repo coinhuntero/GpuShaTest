18a15af06cd2a15e877352a9bcb5d598f6fbcb1a
Jonano <jonano614@mail.ru>
2018-02-03 23:34:12 +0300

    CL kernel optimization step 2

--- GpuSha256/Cpu/sha256_gsim.cpp
+++ GpuSha256/Cpu/sha256_gsim.cpp
@@ -10,10 +10,28 @@
 #define OUTPUT_SIZE 256
 #define OUTPUT_MASK 255
 
-int rotate(int x, int n)
+uint rotate(uint x, uint n)
 {
     return (x << n) | (x >> (32 - n));
 }
+
+//uint inline bytereverse(uint x)
+//{
+//    return (((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24));
+//}
+//
+//uint rot(uint x, uint y)
+//{
+//    return rotate(x, (uint)y);
+//}
+//
+//void inline sharound(uint a, uint b, uint c, uint& d, uint e, uint f, uint g, uint& h, uint x, uint K)
+//{
+//    uint t1 = h + (rot(e, 26) ^ rot(e, 21) ^ rot(e, 7)) + (g ^ (e&(f^g))) + K + x;
+//    uint t2 = (rot(a, 30) ^ rot(a, 19) ^ rot(a, 10)) + ((a&b) | (c&(a | b)));
+//    d += t1;
+//    h = t1 + t2;
+//}
 
 void DumpHex(uint *byteArray, uint length)
 {
@@ -35,28 +53,10 @@ void DumpHex(uint *byteArray, uint length)
 
 namespace gsim
 {
-//#define bytereverse(x) ( ((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24) )
-//#define rot(x, y) rotate(x, (uint)y)
+#define bytereverse(x) ( ((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24) )
+#define rot(x, y) rotate(x, (uint)y)
 #define R(x) (work[x] = (rot(work[x - 2], 15) ^ rot(work[x - 2], 13) ^ ((work[x - 2] & 0xffffffff) >> 10)) + work[x - 7] + (rot(work[x - 15], 25) ^ rot(work[x - 15], 14) ^ ((work[x - 15] & 0xffffffff) >> 3)) + work[x - 16])
-//#define sharound(a, b, c, d, e, f, g, h, x, K) { t1 = h + (rot(e, 26) ^ rot(e, 21) ^ rot(e, 7)) + (g ^ (e&(f^g))) + K + x; t2 = (rot(a, 30) ^ rot(a, 19) ^ rot(a, 10)) + ((a&b) | (c&(a | b))); d += t1; h = t1 + t2; }
-    uint inline bytereverse(uint x)
-    {
-        return (((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24));
-    }
-
-    uint rot(uint x, uint y)
-    {
-        return rotate(x, (uint)y);
-    }
-
-    void inline sharound(uint a, uint b, uint c, uint& d, uint e, uint f, uint g, uint& h, uint x, uint K)
-    { 
-        uint t1 = h + (rot(e, 26) ^ rot(e, 21) ^ rot(e, 7)) + (g ^ (e&(f^g))) + K + x;
-        uint t2 = (rot(a, 30) ^ rot(a, 19) ^ rot(a, 10)) + ((a&b) | (c&(a | b)));
-        d += t1; 
-        h = t1 + t2; 
-    }
-    
+#define sharound(a, b, c, d, e, f, g, h, x, K) { t1 = h + (rot(e, 26) ^ rot(e, 21) ^ rot(e, 7)) + (g ^ (e&(f^g))) + K + x; t2 = (rot(a, 30) ^ rot(a, 19) ^ rot(a, 10)) + ((a&b) | (c&(a | b))); d += t1; h = t1 + t2; }
 
 	int cmphash(uint *l, uint *r)
 	{
@@ -83,6 +83,7 @@ namespace gsim
 
         uint work[64];
         uint A, B, C, D, E, F, G, H;
+        uint A2, B2, C2, D2, E2, F2, G2, H2;
         uint t1, t2;
 
         A = state[0];
@@ -94,38 +95,39 @@ namespace gsim
         G = state[6];
         H = state[7];
 
-        work[0] = data[0];
-        work[1] = data[1];
-        work[2] = data[2];
-        work[3] = data[3];
-        work[4] = data[4];
-        work[5] = data[5];
-        work[6] = data[6];
-        work[7] = data[7];
-        work[8] = data[8];
-        work[9] = data[9];
-        work[10] = data[10];
-        work[11] = data[11];
-        work[12] = data[12];
-        work[13] = data[13];
-        ((ulong*)work)[7] = nonce;
+        work[0] = bytereverse(data[0]);
+        work[1] = bytereverse(data[1]);
+        work[2] = bytereverse(data[2]);
+        work[3] = bytereverse(data[3]);
+        work[4] = bytereverse(data[4]);
+        work[5] = bytereverse(data[5]);
+        work[6] = bytereverse(data[6]);
+        work[7] = bytereverse(data[7]);
+        work[8] = bytereverse(data[8]);
+        work[9] = bytereverse(data[9]);
+        work[10] = bytereverse(data[10]);
+        work[11] = bytereverse(data[11]);
+        work[12] = bytereverse(data[12]);
+        work[13] = bytereverse(data[13]);
+        work[14] = bytereverse((uint)nonce);
+        work[15] = bytereverse((uint)(nonce >> 32));
 
-        sharound(A, B, C, D, E, F, G, H, bytereverse(work[0]), 0x428A2F98);
-        sharound(H, A, B, C, D, E, F, G, bytereverse(work[1]), 0x71374491);
-        sharound(G, H, A, B, C, D, E, F, bytereverse(work[2]), 0xB5C0FBCF);
-        sharound(F, G, H, A, B, C, D, E, bytereverse(work[3]), 0xE9B5DBA5);
-        sharound(E, F, G, H, A, B, C, D, bytereverse(work[4]), 0x3956C25B);
-        sharound(D, E, F, G, H, A, B, C, bytereverse(work[5]), 0x59F111F1);
-        sharound(C, D, E, F, G, H, A, B, bytereverse(work[6]), 0x923F82A4);
-        sharound(B, C, D, E, F, G, H, A, bytereverse(work[7]), 0xAB1C5ED5);
-        sharound(A, B, C, D, E, F, G, H, bytereverse(work[8]), 0xD807AA98);
-        sharound(H, A, B, C, D, E, F, G, bytereverse(work[9]), 0x12835B01);
-        sharound(G, H, A, B, C, D, E, F, bytereverse(work[10]), 0x243185BE);
-        sharound(F, G, H, A, B, C, D, E, bytereverse(work[11]), 0x550C7DC3);
-        sharound(E, F, G, H, A, B, C, D, bytereverse(work[12]), 0x72BE5D74);
-        sharound(D, E, F, G, H, A, B, C, bytereverse(work[13]), 0x80DEB1FE);
-        sharound(C, D, E, F, G, H, A, B, bytereverse(work[14]), 0x9BDC06A7);
-        sharound(B, C, D, E, F, G, H, A, bytereverse(work[15]), 0xC19BF174);
+        sharound(A, B, C, D, E, F, G, H, work[0], 0x428A2F98);
+        sharound(H, A, B, C, D, E, F, G, work[1], 0x71374491);
+        sharound(G, H, A, B, C, D, E, F, work[2], 0xB5C0FBCF);
+        sharound(F, G, H, A, B, C, D, E, work[3], 0xE9B5DBA5);
+        sharound(E, F, G, H, A, B, C, D, work[4], 0x3956C25B);
+        sharound(D, E, F, G, H, A, B, C, work[5], 0x59F111F1);
+        sharound(C, D, E, F, G, H, A, B, work[6], 0x923F82A4);
+        sharound(B, C, D, E, F, G, H, A, work[7], 0xAB1C5ED5);
+        sharound(A, B, C, D, E, F, G, H, work[8], 0xD807AA98);
+        sharound(H, A, B, C, D, E, F, G, work[9], 0x12835B01);
+        sharound(G, H, A, B, C, D, E, F, work[10], 0x243185BE);
+        sharound(F, G, H, A, B, C, D, E, work[11], 0x550C7DC3);
+        sharound(E, F, G, H, A, B, C, D, work[12], 0x72BE5D74);
+        sharound(D, E, F, G, H, A, B, C, work[13], 0x80DEB1FE);
+        sharound(C, D, E, F, G, H, A, B, work[14], 0x9BDC06A7);
+        sharound(B, C, D, E, F, G, H, A, work[15], 0xC19BF174);
         sharound(A, B, C, D, E, F, G, H, R(16), 0xE49B69C1);
         sharound(H, A, B, C, D, E, F, G, R(17), 0xEFBE4786);
         sharound(G, H, A, B, C, D, E, F, R(18), 0x0FC19DC6);
@@ -175,7 +177,16 @@ namespace gsim
         sharound(C, D, E, F, G, H, A, B, R(62), 0xBEF9A3F7);
         sharound(B, C, D, E, F, G, H, A, R(63), 0xC67178F2);
 
-        work[0] = 0x80;
+        A2 = A = A + state[0];
+        B2 = B = B + state[1];
+        C2 = C = C + state[2];
+        D2 = D = D + state[3];
+        E2 = E = E + state[4];
+        F2 = F = F + state[5];
+        G2 = G = G + state[6];
+        H2 = H = H + state[7];
+
+        work[0] = 0x80000000;
         work[1] = 0;
         work[2] = 0;
         work[3] = 0;
@@ -190,24 +201,24 @@ namespace gsim
         work[12] = 0;
         work[13] = 0;
         work[14] = 0;
-        work[15] = 0x00100000;
+        work[15] = 0x00001000;
 
-        sharound(A, B, C, D, E, F, G, H, bytereverse(work[0]), 0x428A2F98);
-        sharound(H, A, B, C, D, E, F, G, bytereverse(work[1]), 0x71374491);
-        sharound(G, H, A, B, C, D, E, F, bytereverse(work[2]), 0xB5C0FBCF);
-        sharound(F, G, H, A, B, C, D, E, bytereverse(work[3]), 0xE9B5DBA5);
-        sharound(E, F, G, H, A, B, C, D, bytereverse(work[4]), 0x3956C25B);
-        sharound(D, E, F, G, H, A, B, C, bytereverse(work[5]), 0x59F111F1);
-        sharound(C, D, E, F, G, H, A, B, bytereverse(work[6]), 0x923F82A4);
-        sharound(B, C, D, E, F, G, H, A, bytereverse(work[7]), 0xAB1C5ED5);
-        sharound(A, B, C, D, E, F, G, H, bytereverse(work[8]), 0xD807AA98);
-        sharound(H, A, B, C, D, E, F, G, bytereverse(work[9]), 0x12835B01);
-        sharound(G, H, A, B, C, D, E, F, bytereverse(work[10]), 0x243185BE);
-        sharound(F, G, H, A, B, C, D, E, bytereverse(work[11]), 0x550C7DC3);
-        sharound(E, F, G, H, A, B, C, D, bytereverse(work[12]), 0x72BE5D74);
-        sharound(D, E, F, G, H, A, B, C, bytereverse(work[13]), 0x80DEB1FE);
-        sharound(C, D, E, F, G, H, A, B, bytereverse(work[14]), 0x9BDC06A7);
-        sharound(B, C, D, E, F, G, H, A, bytereverse(work[15]), 0xC19BF174);
+        sharound(A, B, C, D, E, F, G, H, work[0], 0x428A2F98);
+        sharound(H, A, B, C, D, E, F, G, work[1], 0x71374491);
+        sharound(G, H, A, B, C, D, E, F, work[2], 0xB5C0FBCF);
+        sharound(F, G, H, A, B, C, D, E, work[3], 0xE9B5DBA5);
+        sharound(E, F, G, H, A, B, C, D, work[4], 0x3956C25B);
+        sharound(D, E, F, G, H, A, B, C, work[5], 0x59F111F1);
+        sharound(C, D, E, F, G, H, A, B, work[6], 0x923F82A4);
+        sharound(B, C, D, E, F, G, H, A, work[7], 0xAB1C5ED5);
+        sharound(A, B, C, D, E, F, G, H, work[8], 0xD807AA98);
+        sharound(H, A, B, C, D, E, F, G, work[9], 0x12835B01);
+        sharound(G, H, A, B, C, D, E, F, work[10], 0x243185BE);
+        sharound(F, G, H, A, B, C, D, E, work[11], 0x550C7DC3);
+        sharound(E, F, G, H, A, B, C, D, work[12], 0x72BE5D74);
+        sharound(D, E, F, G, H, A, B, C, work[13], 0x80DEB1FE);
+        sharound(C, D, E, F, G, H, A, B, work[14], 0x9BDC06A7);
+        sharound(B, C, D, E, F, G, H, A, work[15], 0xC19BF174);
         sharound(A, B, C, D, E, F, G, H, R(16), 0xE49B69C1);
         sharound(H, A, B, C, D, E, F, G, R(17), 0xEFBE4786);
         sharound(G, H, A, B, C, D, E, F, R(18), 0x0FC19DC6);
@@ -259,21 +270,21 @@ namespace gsim
 
         // hash the hash now
 
-        work[0] = state[0] + A;
-        work[1] = state[1] + B;
-        work[2] = state[2] + C;
-        work[3] = state[3] + D;
-        work[4] = state[4] + E;
-        work[5] = state[5] + F;
-        work[6] = state[6] + G;
-        work[7] = state[7] + H;
+        work[0] = A2 + A;
+        work[1] = B2 + B;
+        work[2] = C2 + C;
+        work[3] = D2 + D;
+        work[4] = E2 + E;
+        work[5] = F2 + F;
+        work[6] = G2 + G;
+        work[7] = H2 + H;
         work[8] = 0x80000000;
-        work[9] = 0x00000000;
-        work[10] = 0x00000000;
-        work[11] = 0x00000000;
-        work[12] = 0x00000000;
-        work[13] = 0x00000000;
-        work[14] = 0x00000000;
+        work[9] = 0;
+        work[10] = 0;
+        work[11] = 0;
+        work[12] = 0;
+        work[13] = 0;
+        work[14] = 0;
         work[15] = 0x00000100;
 
         A = 0x6a09e667;
@@ -285,22 +296,22 @@ namespace gsim
         G = 0x1f83d9ab;
         H = 0x5be0cd19;
 
-        sharound(A, B, C, D, E, F, G, H, bytereverse(work[0]), 0x428A2F98);
-        sharound(H, A, B, C, D, E, F, G, bytereverse(work[1]), 0x71374491);
-        sharound(G, H, A, B, C, D, E, F, bytereverse(work[2]), 0xB5C0FBCF);
-        sharound(F, G, H, A, B, C, D, E, bytereverse(work[3]), 0xE9B5DBA5);
-        sharound(E, F, G, H, A, B, C, D, bytereverse(work[4]), 0x3956C25B);
-        sharound(D, E, F, G, H, A, B, C, bytereverse(work[5]), 0x59F111F1);
-        sharound(C, D, E, F, G, H, A, B, bytereverse(work[6]), 0x923F82A4);
-        sharound(B, C, D, E, F, G, H, A, bytereverse(work[7]), 0xAB1C5ED5);
-        sharound(A, B, C, D, E, F, G, H, bytereverse(work[8]), 0xD807AA98);
-        sharound(H, A, B, C, D, E, F, G, bytereverse(work[9]), 0x12835B01);
-        sharound(G, H, A, B, C, D, E, F, bytereverse(work[10]), 0x243185BE);
-        sharound(F, G, H, A, B, C, D, E, bytereverse(work[11]), 0x550C7DC3);
-        sharound(E, F, G, H, A, B, C, D, bytereverse(work[12]), 0x72BE5D74);
-        sharound(D, E, F, G, H, A, B, C, bytereverse(work[13]), 0x80DEB1FE);
-        sharound(C, D, E, F, G, H, A, B, bytereverse(work[14]), 0x9BDC06A7);
-        sharound(B, C, D, E, F, G, H, A, bytereverse(work[15]), 0xC19BF174);
+        sharound(A, B, C, D, E, F, G, H, work[0], 0x428A2F98);
+        sharound(H, A, B, C, D, E, F, G, work[1], 0x71374491);
+        sharound(G, H, A, B, C, D, E, F, work[2], 0xB5C0FBCF);
+        sharound(F, G, H, A, B, C, D, E, work[3], 0xE9B5DBA5);
+        sharound(E, F, G, H, A, B, C, D, work[4], 0x3956C25B);
+        sharound(D, E, F, G, H, A, B, C, work[5], 0x59F111F1);
+        sharound(C, D, E, F, G, H, A, B, work[6], 0x923F82A4);
+        sharound(B, C, D, E, F, G, H, A, work[7], 0xAB1C5ED5);
+        sharound(A, B, C, D, E, F, G, H, work[8], 0xD807AA98);
+        sharound(H, A, B, C, D, E, F, G, work[9], 0x12835B01);
+        sharound(G, H, A, B, C, D, E, F, work[10], 0x243185BE);
+        sharound(F, G, H, A, B, C, D, E, work[11], 0x550C7DC3);
+        sharound(E, F, G, H, A, B, C, D, work[12], 0x72BE5D74);
+        sharound(D, E, F, G, H, A, B, C, work[13], 0x80DEB1FE);
+        sharound(C, D, E, F, G, H, A, B, work[14], 0x9BDC06A7);
+        sharound(B, C, D, E, F, G, H, A, work[15], 0xC19BF174);
         sharound(A, B, C, D, E, F, G, H, R(16), 0xE49B69C1);
         sharound(H, A, B, C, D, E, F, G, R(17), 0xEFBE4786);
         sharound(G, H, A, B, C, D, E, F, R(18), 0x0FC19DC6);
@@ -354,14 +365,14 @@ namespace gsim
 
         ////////////////
 		output[0] = nonce;
-        outputHash[0] = bytereverse(A);
-        outputHash[1] = bytereverse(B);
-        outputHash[2] = bytereverse(C);
-        outputHash[3] = bytereverse(D);
-        outputHash[4] = bytereverse(E);
-        outputHash[5] = bytereverse(F);
-        outputHash[6] = bytereverse(G);
-        outputHash[7] = bytereverse(H);
+        outputHash[0] = bytereverse(A + 0x6a09e667);
+        outputHash[1] = bytereverse(B + 0xbb67ae85);
+        outputHash[2] = bytereverse(C + 0x3c6ef372);
+        outputHash[3] = bytereverse(D + 0xa54ff53a);
+        outputHash[4] = bytereverse(E + 0x510e527f);
+        outputHash[5] = bytereverse(F + 0x9b05688c);
+        outputHash[6] = bytereverse(G + 0x1f83d9ab);
+        outputHash[7] = bytereverse(H + 0x5be0cd19);
 	}
 
     void search_nonce2(uint const* hashState,
